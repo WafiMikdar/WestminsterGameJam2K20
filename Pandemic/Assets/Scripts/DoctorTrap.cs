53125ea4afe37f0ce0df88227fc732827eab0347
jasninus <jasninus8@hotmail.dk>
2020-02-27 16:18:51 +0100

    Place traps

---
+++ Pandemic/Assets/Scripts/DoctorTrap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoctorTrap : MonoBehaviour
+{
+    [SerializeField] private float activationDistance;
+    private float sqrActivationDistance;
+
+    private Transform monsterTransform;
+
+    private void Awake()
+    {
+        monsterTransform = GameObject.FindWithTag("Monster").transform;
+        sqrActivationDistance = activationDistance * activationDistance;
+    }
+
+    private void Update()
+    {
+        if ((monsterTransform.position - transform.position).sqrMagnitude <= sqrActivationDistance)
+        {
+            Activate();
+        }
+    }
+
+    private void Activate()
+    {
+        //monsterTransform
+    }
+}
---
+++ Pandemic/Assets/Scripts/DoctorTrapPlacer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoctorTrapPlacer : MonoBehaviour
+{
+    [SerializeField] private float cooldown;
+    private float availableTime;
+
+    [SerializeField] private GameObject trap;
+
+    private void Update()
+    {
+        if (CanPlaceTrap())
+        {
+            PlaceTrap();
+        }
+    }
+
+    private bool CanPlaceTrap()
+    {
+        return Input.GetKeyDown(KeyCode.T) &&
+               Time.time >= availableTime;
+    }
+
+    private void PlaceTrap()
+    {
+        Instantiate(trap, transform.position, Quaternion.identity);
+        availableTime = Time.time + cooldown;
+    }
+}
