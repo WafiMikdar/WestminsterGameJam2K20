16301b1c3a4161f77d2529a84078618d70486a1d
Jan Mark <jan.mark2771@gmail.com>
2020-02-28 15:06:14 +0000

    playeranimation

--- Pandemic/Assets/Scripts/DoctorControls.cs
+++ Pandemic/Assets/Scripts/DoctorControls.cs
@@ -9,19 +9,54 @@ public class DoctorControls : MonoBehaviour
     private Vector2 doctorVelocity;
     private DoctorAdrenalinBoost dab;
 
-    public float Speed { get => speed; set => speed = value; }
+    [SerializeField] private DoctorCuring curing;
+    [SerializeField] private DoctorTrapPlacer trapPlacer;
+    [SerializeField] private MotionSensorPlacer sensorPlacer;
 
+    public float Speed { get => speed; set => speed = value; }
+    public Animator anim;
+    private SpriteRenderer spR;
+    public float vel;
+    private void Start()
+    {
+        spR = GetComponent<SpriteRenderer>();
+    }
 
     private void Move()
     {
         Vector2 movementVector2 = new Vector2(doctorVelocity.x, doctorVelocity.y) * speed * Time.deltaTime;
         transform.Translate(movementVector2);
-        if (doctorVelocity.y > movementVector2.y) { Debug.Log("up"); }
-        if (doctorVelocity.y < movementVector2.y) { Debug.Log("down"); }
-        if (doctorVelocity.x < movementVector2.x) { Debug.Log("left"); }
-        if (doctorVelocity.x > movementVector2.x) { Debug.Log("right"); }
-    }
+        if(doctorVelocity.x < 0)
+        {
+            spR.flipX = true;
+        }
+        else if (doctorVelocity.x > 0)
+        {
+            spR.flipX = false;
+        }
+        vel = Mathf.Sqrt(Mathf.Pow(movementVector2.x, 2) + Mathf.Pow(movementVector2.y, 2));
+        Debug.Log(vel);
+        if (doctorVelocity.y > movementVector2.y) { 
+            Debug.Log("up");
+            anim.Play("Run");
+        }
+        anim.SetFloat("Vel", vel);
+        if (doctorVelocity.y < movementVector2.y) { 
+            Debug.Log("down");
+            anim.Play("Run");
+        }
 
+        if (doctorVelocity.x < movementVector2.x) {
+            Debug.Log("left");
+            anim.Play("Run");
+        }
+
+        if (doctorVelocity.x > movementVector2.x) {
+            Debug.Log("right");
+            anim.Play("Run");
+        }
+        
+    }
     private void OnMove(InputValue value)
     {
         doctorVelocity = value.Get<Vector2>();
@@ -29,17 +64,17 @@ public class DoctorControls : MonoBehaviour
 
     private void OnAttack()
     {
-        Debug.Log("Attacked");
+        curing.TryCure();
     }
 
     private void OnAbilityOne()
     {
-        Debug.Log("Ability One");
+        trapPlacer.TryPlaceTrap();
     }
 
     private void OnAbilityTwo()
     {
-        Debug.Log("Ability One");
+        sensorPlacer.TryPlaceSensor();
     }
 
     private void OnAbilityThree()
