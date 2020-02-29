c7a55974cb545cf91ed19244f065b04047dc3e23
jasninus <jasninus8@hotmail.dk>
2020-02-29 11:27:12 +0100

    Made HumanMovement class

---
+++ Pandemic/Assets/Scripts/HumanMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HumanHealth))]
+public class HumanMovement : MonoBehaviour
+{
+    [SerializeField] private float healthySpeed, infectedSpeed;
+
+    private HumanHealth health;
+
+    private Transform monsterTransform;
+
+    private void Awake()
+    {
+        health = GetComponent<HumanHealth>();
+        monsterTransform = GameObject.FindWithTag("Monster").transform;
+    }
+}
