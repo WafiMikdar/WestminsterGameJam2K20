3deb4722a574cbf9a1256e5e01aab9fcce50e239
loguntetley <48089813+loguntetley@users.noreply.github.com>
2020-03-01 11:35:56 +0000

    Corrected Audio

--- Pandemic/Assets/Scripts/MonsterProgression.cs
+++ Pandemic/Assets/Scripts/MonsterProgression.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(Experience))]
 public class MonsterProgression : MonoBehaviour
 {
+
+    [SerializeField] private MonsterSfx monsterSfx;
     private void Awake()
     {
         GetComponent<Experience>().onLevelUp += LevelUp;
+        monsterSfx.PlaySFX(monsterSfx.MonsterEvolution);
     }
 
     private void LevelUp(uint newLevel)
