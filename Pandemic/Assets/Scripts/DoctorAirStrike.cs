e6a7313febba0b1f6ada2846b9ad68c44ce91694
jasninus <jasninus8@hotmail.dk>
2020-03-01 14:23:00 +0100

    Air strike functionality

--- Pandemic/Assets/Scripts/DoctorAirStrike.cs
+++ Pandemic/Assets/Scripts/DoctorAirStrike.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(Experience))]
 public class DoctorAirStrike : SupplyDropAbility
 {
     [SerializeField] private GameObject indicator;
+
+    [SerializeField] private Sprite dangerSprite;
 
     [SerializeField] private ParticleSystem horizontalParticles, verticalParticles;
 
     [SerializeField] private Vector2 size, tileSize;
 
-    [SerializeField] private float warningDuration, minIndicatorAlpha, maxIndicatorAlpha, frequencyModifierFactor, frequencyModifierExponent;
+    [SerializeField] private float warningDuration, dangerDuration, minIndicatorAlpha, maxIndicatorAlpha, baseFrequencyModifier, frequencyModifierFactor, frequencyModifierExponent;
+
+    private Experience experience;
+
+    private void Awake()
+    {
+        experience = GetComponent<Experience>();
+        Activate();
+    }
 
     public override void TryActivate()
     {
@@ -21,19 +33,19 @@ public class DoctorAirStrike : SupplyDropAbility
     public override void Activate()
     {
         Vector2 intersection = GetNearestTileIntersection();
-        SpriteRenderer horizontalRend = Instantiate(indicator, intersection, Quaternion.Euler(0, 0, 90)).GetComponent<SpriteRenderer>();
+        SpriteRenderer horizontalRend = Instantiate(indicator, intersection, Quaternion.Euler(0, 0, 0)).GetComponent<SpriteRenderer>();
         SpriteRenderer verticalRend = Instantiate(indicator, intersection, Quaternion.identity).GetComponent<SpriteRenderer>();
 
-        horizontalParticles.transform.position = intersection;
-        verticalParticles.transform.position = intersection;
+        //horizontalParticles.transform.position = intersection;
+        //verticalParticles.transform.position = intersection;
         horizontalRend.size = size;
-        verticalRend.size = size;
+        verticalRend.size = new Vector2(size.y, size.x);
 
         StartCoroutine(FlashingWarningZone(horizontalRend, verticalRend));
     }
 
-    private Vector2 GetNearestTileIntersection() => new Vector2((int)(transform.position.x / tileSize.x) * tileSize.x,
-                                                                (int)(transform.position.y / tileSize.y) * tileSize.y);
+    private Vector2 GetNearestTileIntersection() => new Vector2(Mathf.RoundToInt(transform.position.x / tileSize.x) * tileSize.x,
+                                                                Mathf.RoundToInt(transform.position.y / tileSize.y) * tileSize.y);
 
     private IEnumerator FlashingWarningZone(SpriteRenderer horizontalRend, SpriteRenderer verticalRend)
     {
@@ -42,21 +54,45 @@ public class DoctorAirStrike : SupplyDropAbility
         while (endTime > Time.time)
         {
             float alpha = GetIndicatorAlpha(startTime);
+            Debug.Log($"Alpha: {alpha}");
             horizontalRend.color = new Color(horizontalRend.color.r, horizontalRend.color.g, horizontalRend.color.b, alpha);
             verticalRend.color = new Color(verticalRend.color.r, verticalRend.color.g, verticalRend.color.b, alpha);
             yield return null;
         }
 
-        Destroy(horizontalRend.gameObject);
-        Destroy(verticalRend.gameObject);
-        horizontalParticles.Play(true);
-        verticalParticles.Play(true);
+        StartCoroutine(WarningZone(horizontalRend, verticalRend));
     }
 
     private float GetIndicatorAlpha(float startTime)
     {
         float elapsed = Time.time - startTime;
-        float sin = Mathf.Sin(elapsed + frequencyModifierFactor * Mathf.Pow(elapsed, frequencyModifierExponent));
+        float sin = Mathf.Sin(baseFrequencyModifier * elapsed + frequencyModifierFactor * Mathf.Pow(elapsed, frequencyModifierExponent));
         return Mathf.Lerp(minIndicatorAlpha, maxIndicatorAlpha, sin * 0.5f + 0.5f);
+    }
+
+    private IEnumerator WarningZone(SpriteRenderer horizontalRend, SpriteRenderer verticalRend)
+    {
+        horizontalRend.sprite = dangerSprite;
+        verticalRend.sprite = dangerSprite;
+
+        yield return new WaitForSeconds(dangerDuration);
+
+        CureInStrikeZone(horizontalRend, verticalRend);
+
+        Destroy(horizontalRend.gameObject);
+        Destroy(verticalRend.gameObject);
+        horizontalParticles?.Play(true);
+        verticalParticles?.Play(true);
+    }
+
+    private void CureInStrikeZone(SpriteRenderer horizontalRend, SpriteRenderer verticalRend)
+    {
+        Collider2D[] colls = Physics2D.OverlapBoxAll(verticalRend.transform.position, verticalRend.size, 0).Union(
+            Physics2D.OverlapBoxAll(horizontalRend.transform.position, horizontalRend.size, 0)).ToArray();
+
+        foreach (Collider2D coll in colls)
+        {
+            coll.GetComponent<ICurable>()?.Cure(experience);
+        }
     }
 }
